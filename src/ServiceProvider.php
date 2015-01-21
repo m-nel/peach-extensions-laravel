7f75f47785c6c7bc0a97392adb4bbdb7c7fd5579
Michael Nel <michael.nel@mrd.com>
2015-01-21 09:53:11 +0200

    Added bindings for Client

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
+use Mnel\Peach\Client\Client;
+use Mnel\Peach\Client\GuzzleClient;
+use Mnel\Peach\Client\GuzzleResponseFormatter;
+use Mnel\Peach\Client\XmlGuzzleResponseFormatter;
 use Mnel\Peach\Commands\CommandBus;
 use Mnel\Peach\Commands\CommandHandlerTransalator;
 use Mnel\Peach\Commands\DefaultCommandBus;
@@ -16,8 +20,12 @@ class ServiceProvider extends LaravelServiceProvider
 {
     public function register()
     {
+        $this->app->bind(Client::class, GuzzleClient::class);
+        $this->app->bind(GuzzleResponseFormatter::class, XmlGuzzleResponseFormatter::class);
+
         $this->app->bind(QueryRequestTransformer::class, XmlQueryRequestTransformer::class);
         $this->app->bind(QueryResponseTransformer::class, XmlQueryResponseTransformer::class);
+
         $this->app->bind(CommandBus::class, DefaultCommandBus::class);
         $this->app->bind(CommandHandlerTransalator::class, LaravelCommandHandlerTransalator::class);
     }
